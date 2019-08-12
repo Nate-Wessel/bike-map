8791502f58a7eac3076e71d1f2953bb0c606b2ff
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-08-12 16:58:46 -0400

    synthesize trips with random point placement

---
+++ synthesize-trips-from-flow-matrix.sql
@@ -0,0 +1,19 @@
+﻿WITH sub AS (
+	SELECT 
+		orig,
+		dest,
+		(ST_Dump(ST_GeneratePoints(oz.geom,total_trips))).geom AS ogeom,
+		(ST_Dump(ST_GeneratePoints(dz.geom,total_trips))).geom AS dgeom
+	FROM tts_trips AS t
+	JOIN tts_2006_zones AS oz
+		ON t.orig = oz.zone_id
+	JOIN tts_2006_zones AS dz
+		ON t.dest = dz.zone_id
+	WHERE total_trips > 0
+)
+SELECT 
+	orig, 
+	dest, 
+	ST_MakeLine(ogeom,dgeom)
+INTO gen_trips
+FROM sub;
