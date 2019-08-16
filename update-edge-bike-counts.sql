312a25616906942a63240fa7244ba45f7e9110f8
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-08-16 13:52:13 -0400

    add edge counts to table from CSV

---
+++ update-edge-bike-counts.sql
@@ -0,0 +1,16 @@
+﻿-- create temporary table with edge counts from python script
+DROP TABLE IF EXISTS edge_counts;
+CREATE TABLE edge_counts (
+	n1 bigint,
+	n2 bigint,
+	count integer
+);
+COPY edge_counts (n1,n2,count) 
+FROM '/home/nate/bike-map/nodepairs.csv' CSV HEADER;
+
+UPDATE gta_edges SET bike_count = count
+FROM edge_counts
+WHERE ARRAY[n1,n2] = nodes;
+
+-- drop the temp table
+DROP TABLE edge_counts;
