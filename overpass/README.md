1bf6a9ca1d7d45e3ca464f3fffa4074b0b9fa854
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-09-05 15:17:20 -0400

    reorganizing data

---
+++ overpass/README.md
@@ -0,0 +1,14 @@
+These query definitions are means of gathering specific subsets of data from 
+OpenStreetMap using the 
+[Overpass API](https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL). 
+
+One is used to gather highway data and a separate one for data whch is not used 
+for routing but which may provide cartographic context. 
+
+I've been using several bounding boxes for testing queries:
+
+````
+toronto plus a bit: 43.5,-80,44,-79
+parkdale:           43.64,-79.44,43.65,-79.43
+full GTHA:          42.8,-80.8,45.0,-77.9
+````
--- update-data.sh
+++ update-data.sh
@@ -4,8 +4,14 @@ cd
 # get new street data from the overpass API
 wget -O scripts/osrm-backend/osm-data/gta.osm --post-file=bike-map/overpass-ways-query.txt https://overpass-api.de/api/interpreter
 
-# import it into postGIS
-osm2pgsql --slim --hstore-all --prefix gta -d bikemap --style bike-map/osm2pgsql.style scripts/osrm-backend/osm-data/gta.osm
+# import street data into postGIS
+osm2pgsql --slim --hstore-all --prefix gta -d bikemap --style bike-map/osm2pgsql/ways.style scripts/osrm-backend/osm-data/gta.osm
+
+# get new landuse/context data from the overpass API
+wget -O bike-map/other-data/context.osm --post-file=bike-map/overpass-context-query.txt https://overpass-api.de/api/interpreter
+
+# import context data into postGIS
+osm2pgsql --slim --hstore-all --prefix context -d bikemap --style bike-map/osm2pgsql/context.style bike-map/other-data/context.osm
 
 # this makes a later query a little faster
 psql -d bikemap -c "CREATE INDEX ON gta_nodes (id);"
