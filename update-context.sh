29daf2153448b892c271b96bbcd5e3618b3a2f25
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-09-12 19:45:29 -0400

    renamed other-data to data, storing osm data

--- update-context.sh
+++ update-context.sh
@@ -1,10 +1,7 @@
 cd ~/bike-map
 
 # get new landuse/context data from the overpass API
-wget -O other-data/context.osm --post-file=overpass/context.txt https://overpass-api.de/api/interpreter
+wget -O data/context.osm --post-file=overpass/context.txt https://overpass-api.de/api/interpreter
 
 # import data into postGIS, overwriting old data
-osm2pgsql --slim --hstore-all --prefix context -d bikemap --style osm2pgsql/context.style other-data/context.osm
-
-# clear out the OSM data once it's in PostGIS
-rm other-data/context.osm
+osm2pgsql --slim --hstore-all --prefix context -d bikemap --style osm2pgsql/context.style data/context.osm
