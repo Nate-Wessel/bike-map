991c557e68602c31e92cf1c1cdac798a485c7d89
Nate Wessel <bike756@gmail.com>
2019-09-14 02:23:41 -0400

    script for creating nodes along roads

---
+++ create-nodes.sql
@@ -0,0 +1,13 @@
+﻿/*Synthesize O/D points along roads, to be used in trip generation */
+
+DROP TABLE IF EXISTS syn_points;
+
+SELECT 
+	osm_id,
+	ST_LineInterpolatePoint(way,random()) AS geom,
+	generate_series(1,(ST_Length(way::geography)/100)::int+1) AS way_uid
+INTO syn_points
+FROM gta_line
+WHERE highway IN ('primary','secondary','tertiary','residential','unclassified','path','cycleway');
+
+CREATE INDEX ON syn_points USING GIST (geom);
