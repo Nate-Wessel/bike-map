3d23ea70abd8773e24f72848c4eab5916824d007
Nate Wessel <bike756@gmail.com>
2019-10-02 17:42:52 -0400

    corrected projection inconsistency

--- synthesize-ODs.sql
+++ synthesize-ODs.sql
@@ -4,12 +4,14 @@ DROP TABLE IF EXISTS syn_ods;
 
 SELECT 
 	osm_id,
-	ST_Transform(ST_LineInterpolatePoint(way,random()),4326)::geography AS geog,
-	generate_series(1,(ST_Length(ST_Transform(way,4326)::geography)/100)::int+1) AS way_uid
+	-- geom in local projection
+	ST_Transform(ST_LineInterpolatePoint(way,random()),26917) AS geom,
+	-- generate number of points in proportion to length +1
+	generate_series(1,(ST_Length(ST_Transform(way,26917))/100)::int+1) AS way_uid
 INTO syn_ods
 FROM gta_line
 WHERE highway IN ('primary','secondary','tertiary','residential','unclassified','path','cycleway');
 
 ALTER TABLE syn_ods ADD COLUMN uid serial PRIMARY KEY;
 
-CREATE INDEX ON syn_ods USING GIST (geog);
+CREATE INDEX ON syn_ods USING GIST (geom);
