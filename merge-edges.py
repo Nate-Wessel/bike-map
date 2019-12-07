9670cb3b90eb1678011c6b04a7e1e5d09616a291
Nate Wessel <bike756@gmail.com>
2019-12-06 19:12:32 -0500

    more work on merging edges, still not near ready

--- merge-edges.py
+++ merge-edges.py
@@ -1,7 +1,12 @@
-from DBconnection import cursor
+from DBconnection import connection
+from shapely.geometry import Point, LineString
+from shapely.wkb import loads as loadWKB, dumps as dumpWKB
+
+node_cursor = connection.cursor()
+edge_cursor = connection.cursor()
 
 # get a list of nodes (node_id) with degree = 2
-cursor.execute("""
+node_cursor.execute("""
 	WITH streets AS (
 		SELECT node_1, node_2
 		FROM street_edges 
@@ -21,18 +26,53 @@ cursor.execute("""
 		SELECT nid, COUNT(*) AS degree 
 		FROM nodes GROUP BY nid
 	) SELECT nid FROM node_degree WHERE degree = 2
-	LIMIT 1000;
+	LIMIT 25;
 """)
-nodes = cursor.fetchall()
+nodes = node_cursor.fetchall()
 print(len(nodes),'to check/merge')
 
+keys = ['uid', 'way_id', 'node_1', 'node_2', 'f', 'r', 'edge']
 for node_id, in nodes:
-	print('node_id =',node_id)
-	cursor.execute("""
-		SELECT  *
-		FROM street_edges
-		WHERE node_1 = %(node_id)s OR node_2 = %(node_id)s
+	# get the two edges connected to the given node
+	print('\tnode_id =',node_id)
+	edge_cursor.execute("""
+		SELECT uid, way_id, node_1, node_2, f, r, edge
+		FROM street_edges 
+		WHERE %(node_id)s IN (node_1,node_2)
 		LIMIT 2;
-	""",{'node_id':node_id});
-	print(cursor.fetchall())
-	break
+	""",{ 'node_id': node_id } );
+	# parse as dicts
+	edges = [ dict(zip(keys,edge)) for edge in edge_cursor.fetchall() ]
+	e1,e2 = edges[0],edges[1]
+	# only accept perfect matches
+	if e1['way_id'] != e2['way_id']: continue
+	if e1['f'] != e2['f']: continue
+	if e1['r'] != e2['r']: continue
+	print('\t\tmatch found!')
+	c1 = loadWKB( e1['edge'], hex=True ).coords
+	c2 = loadWKB( e2['edge'], hex=True ).coords
+	if e1['node_2'] == e2['node_1']: 
+		n1,n2 = e1['node_1'],e2['node_2']
+		newGeom = LineString( c1[:len(c1)-1] + list(c2) )
+	elif e2['node_2'] == e1['node_1']:
+		n1,n2 = e2['node_1'],e1['node_2']
+		newGeom = LineString( c2[:len(c1)-1] + list(c1) )
+	else: 
+		print('as yet unhandled exception')
+		break 
+	# insert the new edge and delete the others
+	"""
+		DELETE FROM street_edges WHERE uid IN (%(edge1_id)s,%(edge2_id)s);
+		INSERT INTO street_edges () VALUES ()
+	""",{
+		# to delete
+		'edge1_id':e1['uid'], 'edge2_id':e2['uid'],
+		# to insert
+		'way_id': e1['way_id'],
+		'node_1': n1, 'node_2': n2,
+		'f': e1['f'], 'r': e1['r'],
+		'geom': dumpWKB(newGeom,hex=True)
+	}
+
+
+
