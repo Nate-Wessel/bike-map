c617a5f47c9ff6a2ff7bbf7889cbfe1b3bf308b3
Nate Wessel <bike756@gmail.com>
2019-12-09 13:13:03 -0500

    cleaning up node SELECT, trying to debug

--- merge-edges.py
+++ merge-edges.py
@@ -7,73 +7,63 @@ edge_cursor = connection.cursor()
 
 # get a list of nodes (node_id) with degree = 2
 node_cursor.execute("""
-	WITH streets AS (
-		SELECT node_1, node_2
-		FROM street_edges 
-		WHERE 
-			f+r > 5 OR
-			(highway='path' AND bicycle='designated') OR
-			highway in (
-				'residential','unclassified',
-				'tertiary','secondary','primary',
-				'cycleway','pedestrian'
-			)
-	), nodes AS (
-		SELECT node_1 AS nid FROM streets
+	WITH all_nodes AS (
+		SELECT node_1 AS nid FROM street_edges WHERE render
 		UNION ALL
-		SELECT node_2 AS nid FROM streets
+		SELECT node_2 AS nid FROM street_edges WHERE render
 	), node_degree AS (
 		SELECT nid, COUNT(*) AS degree 
-		FROM nodes GROUP BY nid
+		FROM all_nodes GROUP BY nid
 	) SELECT nid FROM node_degree WHERE degree = 2;
 """)
 nodes = node_cursor.fetchall()
 print(len(nodes),'to check/merge')
 
-keys = ['uid', 'way_id', 'node_1', 'node_2', 'f', 'r', 'edge']
+keys = ['uid','way_id','node_1','node_2','name','f','r','geom']
 for node_id, in nodes:
 	# get the two edges connected to the given node
-	print('\tnode_id =',node_id)
 	edge_cursor.execute("""
 		SELECT 
-			uid, way_id, node_1, node_2, f, r, edge
+			uid, way_id, node_1, node_2, name, f, r, edge AS geom
 		FROM street_edges 
-		WHERE %(node_id)s IN (node_1,node_2)
-		LIMIT 2;
+		WHERE %(node_id)s IN (node_1,node_2) AND render;
 	""",{ 'node_id': node_id } );
+	assert edge_cursor.rowcount == 2
 	# parse as dicts
-	edges = [ dict(zip(keys,edge)) for edge in edge_cursor.fetchall() ]
-	e1,e2 = edges[0],edges[1]
+	records = [ dict(zip(keys,edge)) for edge in edge_cursor.fetchall() ]
+	r1,r2 = records[0],records[1]
 	# only accept perfect matches
-	if e1['way_id'] != e2['way_id']: continue
-	if e1['f'] != e2['f']: continue
-	if e1['r'] != e2['r']: continue
-	print('\t\tmatch found!')
-	c1 = loadWKB( e1['edge'], hex=True ).coords
-	c2 = loadWKB( e2['edge'], hex=True ).coords
-	if e1['node_2'] == e2['node_1']: 
-		n1,n2 = e1['node_1'],e2['node_2']
-		newGeom = LineString( c1[:len(c1)-1] + list(c2) )
-	elif e2['node_2'] == e1['node_1']:
-		n1,n2 = e2['node_1'],e1['node_2']
-		newGeom = LineString( c2[:len(c1)-1] + list(c1) )
+	if r1['way_id'] != r2['way_id']: continue
+	if r1['f']      != r2['f']:      continue
+	if r1['r']      != r2['r']:      continue
+	print('\tmerging',(r1['name'] if r1['name']!=None else '-'),'on node',node_id)
+	geom1 = loadWKB( r1['geom'], hex=True )
+	geom2 = loadWKB( r2['geom'], hex=True )
+	coords1, coords2 = geom1.coords, geom2.coords
+	if r1['node_2'] == r2['node_1']: 
+		n1,n2 = r1['node_1'],r2['node_2']
+		newGeom = LineString( coords1[:len(coords1)-1] + list(coords2) )
+	elif r2['node_2'] == r1['node_1']:
+		n1,n2 = r2['node_1'],r1['node_2']
+		newGeom = LineString( coords2[:len(coords2)-1] + list(coords1) )
 	else: 
 		print('as yet unhandled exception')
 		# this is because edges currently all go the same way 
 		# because they are from the same original way
 		break 
+	assert len(coords1)+len(coords2)-1 == len(newGeom.coords)
 	# delete the first edge and update the second one
 	edge_cursor.execute("""
-		DELETE FROM street_edges WHERE uid = %(e1id)s;
+		DELETE FROM street_edges WHERE uid = %(edge1id)s;
 		UPDATE street_edges SET 
 			node_1 = %(node_1)s, 
 			node_2 = %(node_2)s,
 			edge = ST_SetSRID(%(geom)s::geometry,3857),
 			renovated = TRUE
-		WHERE uid = %(e2id)s;
+		WHERE uid = %(edge2id)s;
 	""",{
-		'e1id':e1['uid'],
-		'e2id':e2['uid'],
+		'edge1id':r1['uid'],
+		'edge2id':r2['uid'],
 		'node_1': n1, 
 		'node_2': n2,
 		'geom': dumpWKB(newGeom,hex=True)
