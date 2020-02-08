cc9e2f5f4d7f591bde939877183afdfd60f285eb
Nate Wessel <bike756@gmail.com>
2020-02-08 10:15:12 -0500

    use average cost

--- motorway.sql
+++ motorway.sql
@@ -12,7 +12,7 @@ SELECT
 	node_2 AS target,
 	ST_Length(edge) AS cost,
 	edge AS geom,
-	NULL::real AS dist_from 
+	0::real AS dist_from 
 FROM street_edges 
 WHERE highway IN ('motorway','motorway_link');
 
@@ -33,9 +33,10 @@ JOIN (
 ON mw_nodes.node = s.node;
 
 -- calculate the minimum distance to each segment and update street_edges 
+
 WITH sub AS (
 	SELECT 
-		end_vid,
+		end_vid AS node,
 		MIN(agg_cost) AS min_cost
 	FROM pgr_dijkstraCost(
 		'SELECT id, source, target, cost FROM mw;',
@@ -44,9 +45,18 @@ WITH sub AS (
 		TRUE
 	)
 	GROUP BY end_vid
-) 
-UPDATE mw SET dist_from = min_cost
-FROM sub
-WHERE source = end_vid; 
+), sub2 AS (
+	SELECT 
+		mw.id, mw.source, mw.target, 
+		s1.min_cost, s2.min_cost,
+		(s1.min_cost + s2.min_cost) / 2 AS avg_cost
+	FROM mw 
+	JOIN sub AS s1 ON mw.source = s1.node
+	JOIN sub AS s2 ON mw.target = s2.node
+	ORDER BY id
+)
+UPDATE mw set dist_from = avg_cost
+FROM sub2 WHERE sub2.id = mw.id;
+
 
 
