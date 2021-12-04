812c9335a3c9c576f62955b7c0fbe6ebedd4c9a0
Nate Wessel <bike756@gmail.com>
2021-12-04 09:11:28 -0500

    allow for multiploygon landuses

--- update-context.sh
+++ update-context.sh
@@ -4,7 +4,7 @@ cd ~/bike-map
 wget -O data/context.osm --post-file=overpass/context.txt https://overpass-api.de/api/interpreter
 
 # import data into postGIS, overwriting old data
-osm2pgsql --slim --hstore-all --prefix context -d bikemap --style osm2pgsql/context.style data/context.osm
+osm2pgsql --slim --hstore-all --multi-geometry --prefix context -d bikemap --style osm2pgsql/context.style data/context.osm
 
 # generate random vector points representing e.g wooded areas
 psql -d bikemap -f speckle-wildlands.sql
