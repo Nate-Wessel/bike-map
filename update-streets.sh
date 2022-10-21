f0832d2756a58f02348fd00f205159e76149cb8c
Nate Wessel <bike756@gmail.com>
2022-10-20 22:55:36 -0400

    reorder steps, hide osrm output in log file

--- update-streets.sh
+++ update-streets.sh
@@ -8,9 +8,6 @@ wget -O data/streets.osm --post-file=overpass/ways.overpassql https://overpass-a
 # import way data into postGIS
 osm2pgsql --slim --hstore-all --prefix street -d bikemap --style osm2pgsql/ways.style data/streets.osm
 
-# create edges table
-psql -d bikemap -f create-edge-table-from-osm2pgsql-data.sql
-
 # process the data for OSRM-backend
 mkdir ~/scripts/osrm-backend/osm-data
 cp data/streets.osm ~/scripts/osrm-backend/osm-data/bike.osm
@@ -19,10 +16,10 @@ cd ~/scripts/osrm-backend
 
 build/osrm-extract -p ~/bike-map/osrm-profiles/default-bicycle.lua osm-data/bike.osm
 build/osrm-contract osm-data/bike.osrm
-# this is not needed for routing
+ this is not needed for routing
 rm osm-data/bike.osm
 # run the server in the background but be ready to kill it later
-build/osrm-routed osm-data/bike.osrm & 
+build/osrm-routed osm-data/bike.osrm > ~/bike-map/temp/osrm-output.txt & 
 OSRMserverPID=$!
 
 # return to original folder
@@ -32,6 +29,9 @@ cd ~/bike-map
 psql -d bikemap -f demand/generate-ODs.sql
 python3 demand/generate-trips.py # needs OSRM to be running
 psql -d bikemap -f demand/create-trips-table.sql
+
+# create edges table
+psql -d bikemap -f create-edge-table-from-osm2pgsql-data.sql
 
 # generate betweenness measures and add counts to edges
 python3 between.py
