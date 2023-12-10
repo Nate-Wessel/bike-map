66e7b1f2ce82a9406de674fd0956240fa70a42fd
Nate Wessel <bike756@gmail.com>
2023-12-10 17:45:14 -0500

    indent

--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -12,31 +12,31 @@ protocol.add(p)
 
 // we first fetch the header so we can get the center lon, lat of the map.
 p.getHeader().then( header => {
-    const map = new maplibregl.Map({
-        container: 'map',
-        zoom: header.maxZoom-2,
-        center: [header.centerLon, header.centerLat],
-        style: {
-            version:8,
-            sources: {
-                "example_source": {
-                    type: "vector",
-                    url: `pmtiles://${url}`
-                }
-            },
-            layers: [
-                {
-                    "id":"roads",
-                    "source": "example_source",
-                    "source-layer":"street_edges",
-                    "type": "line",
-                    "paint": {
-                        "line-color": "black"
-                    }
-                }
-            ]
-        }
-    });
-    map.showTileBoundaries = true;
+	const map = new maplibregl.Map({
+		container: 'map',
+		zoom: header.maxZoom - 2,
+		center: [header.centerLon, header.centerLat],
+		style: {
+		version:8,
+		sources: {
+			"example_source": {
+				type: "vector",
+				url: `pmtiles://${url}`
+			}
+		},
+		layers: [
+			{
+				"id":"streets",
+				"source": "example_source",
+				"source-layer":"street_edges",
+				"type": "line",
+				"paint": {
+					"line-color": "black"
+				}
+			}
+		]
+		}
+	})
+	map.showTileBoundaries = true;
 })
 
